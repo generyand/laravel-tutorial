7ec0227a488c78b984cb26df951d20efcca2d768
generyand <generyan.dep@gmail.com>
2025-02-19 22:12:26 +0800

    initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>About Page</x-slot>
+    <h1>Hello, from about</h1>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Contact Page</x-slot>
+    <h1>Hello, from contact</h1>
+</x-layout>
